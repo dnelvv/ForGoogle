77f209c26176566fa911b57c343a6ca77177aefa
Daniil Vaskin <Vaskin.Daniil@wb.ru>
2025-05-07 15:06:42 +0300

    Final change

--- src/test/java/org/example/forgoogle/pages/ResultsPage.java
+++ src/test/java/org/example/forgoogle/pages/ResultsPage.java
@@ -12,30 +12,29 @@ import java.util.List;
 
 public class ResultsPage {
     private WebDriver driver;
+    private WebDriverWait webDriverWait;
 
     @FindBy(css = "#sb_form_q")
     private WebElement searchField;
 
-    @FindBy(xpath = "//a[contains(@class, 'tilk')]")
+    @FindBy(xpath = "//a[contains(@class, 'tilk')][contains(@href, 'selenium.dev')]")
     private List<WebElement> results;
 
     public ResultsPage(WebDriver driver) {
         this.driver = driver;
+        this.webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
         PageFactory.initElements(driver, this);
     }
 
     public void clickElement(int num) {
-        new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.visibilityOf(results.get(num)));
-        new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.elementToBeClickable(results.get(num)))
+        webDriverWait.until(ExpectedConditions.visibilityOf(results.get(num)));
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(results.get(num)))
                 .click();
         System.out.println("Нажатие на результат под номером: " + num);
     }
 
     public String getTextFromSearchField() {
-        new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.visibilityOf(searchField));
+        webDriverWait.until(ExpectedConditions.visibilityOf(searchField));
         String searchValue = searchField.getAttribute("value");
         System.out.println("В строке поиска текст " + searchValue);
         return searchValue;
